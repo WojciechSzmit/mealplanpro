64daf82281824de7084237cee5d8c7415d88f815
WojciechSzmit <studentdbook@gmail.com>
2025-05-25 17:06:09 +0200

    figma

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('searchInput').addEventListener('input', function() {
-    const filter = this.value.toLowerCase();
-    document.querySelectorAll('.post').forEach(post => {
-        const title = post.getAttribute('data-title').toLowerCase();
-        post.style.display = title.includes(filter) ? 'block' : 'none';
-    });
-});
